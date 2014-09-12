07043817ce74a4861a9966918e8891f66c7973a1
dv <wdv4758h@gmail.com>
2014-09-13 00:24:08 +0800

    add Damerau-Levenshtein distance

--- dizzy/_dizzy.py
+++ dizzy/_dizzy.py
@@ -48,3 +48,41 @@ def levenshtein_distance(s1, s2):
 
     else:
         raise ValueError('Unspport types')
+
+def damerau_levenshtein_distance(s1, s2):
+
+    if isinstance(s1, str) and isinstance(s2, str):
+
+        if s1 == s2: return 0
+
+        len1, len2 = len(s1), len(s2)
+
+        if not len1: return len2
+        if not len2: return len1
+
+        if len1 < len2:
+            len1, len2 = len2, len1
+            s1, s2 = s2, s1
+
+        cur = list(range(len2+1))
+        prev = cur.copy()
+
+        for i in range(1, len1+1):
+            pprev, prev = prev, cur.copy()
+            cur[0] = i
+            last = i-1
+
+            for j in range(1, len2+1):
+                old = cur[j]
+                cur[j] = min(cur[j]+1, cur[j-1]+1, last+(s1[i-1]!=s2[j-1]))
+                last = old
+
+                if (i > 1 and j > 1
+                        and s1[i-1] == s2[j-2] and s1[i-2] == s2[j-1]
+                        and s1[i-1] != s2[j-1]):
+                    cur[j] = min(cur[j], pprev[j-2]+1)
+
+        return cur[len2]
+
+    else:
+        raise ValueError('Unspport types')
